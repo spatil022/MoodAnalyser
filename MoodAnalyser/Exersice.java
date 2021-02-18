ac2c373f6b1efa636ef2dbec52f2e39dcdcd1f57
spatil022 <shamalpatil1998@gmail.com>
2021-02-18 19:16:20 +0530

    first commit

---
+++ MoodAnalyser/Exersice.java
@@ -0,0 +1,41 @@
+//import org.junit.Test;
+
+public class Exersice extends Exception {
+
+/**	public class MoodAnalyserException extends Exception {
+		public MoodAnalyserException(String message) {
+			super(message);
+		}
+	}
+	package com.bl.exceptions;
+	public class MoodAnalyser {
+		String message;
+		public MoodAnalyser(String message) {
+			this.message = message;
+		}
+		public static void main(String[] args) {
+			MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+			try {
+				System.out.println(moodAnalyser.analyseMood());
+			} catch (MoodAnalyserException e) {
+				e.printStackTrace();
+			}
+		}
+		public String analyseMood() throws MoodAnalyserException {
+			try {
+				if (message.contains("sad"))
+					return "sad";
+				else
+		
+	else
+					return "Very Happy";
+			} catch (Exception e) {
+				throw new MoodAnalyserException("Please provide valid input");
+			}
+		}**/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
